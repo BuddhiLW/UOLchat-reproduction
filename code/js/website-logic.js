ac6510caf6a8635a17ea7ea8b37ed259bc82478a
BuddhiLW <pedrogbranquinho@gmail.com>
2021-04-15 11:46:49 -0300

    Add components to retrive messages from the server

--- code/js/website-logic.js
+++ code/js/website-logic.js
@@ -11,8 +11,43 @@ var cons = e => c => e.classList.add(c);
 
 let createMsg = message => document.createTextNode(message);
 let appendMsgNode = e => nodeMessage => e.appendChild(nodeMessage);  
-
 let populate = message =>
-    {let complete_li = appendMsgNode(createElement(li))(createMsg(message));
-     populateWith(chat)(complete_li);
+    {let li = createElement('li');
+     appendMsgNode(li)(createMsg(message));
+     chat.appendChild(li);
     };
+
+let handleChat = api =>
+    {let promisseChat = axios.get(api);
+     promisseChat.then(dataPerform);
+     promisseChat.catch(errorHandle);
+    };
+var msgKeys = ["from", "to", "text", "type", "time"];
+let keys = obj => Object.keys(obj);
+let dataPerform = data => {console.log(keys(data.data[0]));
+                           console.log(data.data[0]);
+                          };
+let errorHandle = error => console.log(error);
+handleChat('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
+
+let assignId = e => identity => e.id = `${identity}`;
+let spanNodeId = id =>
+    {let span = createElement('span');
+     assignId(span)(id);
+     return span;
+    };
+let populateNode = node => content => appendMsgNode(node)(createMsg(content));
+let createContent = id => content =>
+    {let span = spanNodeId(id);
+     populateNode(span)(content);
+     return span;
+    };
+
+let add = a => b => a+b;
+let mapKeys = object => {
+    newList = [];
+    Object.entries(object).forEach(e => newList.push(createContent(e[0])(e[1])));
+    return newList;
+};
+
+
